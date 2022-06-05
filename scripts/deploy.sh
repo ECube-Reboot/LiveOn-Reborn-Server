682814549234d348936b905382467ed260fb8696
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-06-05 16:50:52 +0900

    chore: erase unused files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,6 +33,4 @@ echo "> give authority to $JAR_NAME"
 
 chmod +x $JAR_NAME
 
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-real-db.properties,/home/ec2-user/app/application-aws.yml\
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
