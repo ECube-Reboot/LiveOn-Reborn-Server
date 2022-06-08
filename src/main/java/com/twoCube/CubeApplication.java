9293cb6065eb3db813b87c6b7b0ef26486e4001c
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-06-08 11:12:33 +0900

    feat: connect database

--- src/main/java/com/twoCube/CubeApplication.java
+++ src/main/java/com/twoCube/CubeApplication.java
@@ -3,8 +3,10 @@ package com.twoCube;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CubeApplication {
     public static void main(String[] args) {
         SpringApplication.run(CubeApplication.class, args);
--- src/main/java/com/twoCube/gifts/domain/Gift.java
+++ src/main/java/com/twoCube/gifts/domain/Gift.java
@@ -2,12 +2,15 @@ package com.twoCube.gifts.domain;
 
 import com.twoCube.common.domain.BaseTimeEntity;
 import com.twoCube.couple.domain.Couple;
+import com.twoCube.members.domain.Member;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import java.lang.reflect.Member;
+import javax.persistence.*;
 
+@Entity
 public class Gift extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private long gift_id;
     private EGiftType giftType;
