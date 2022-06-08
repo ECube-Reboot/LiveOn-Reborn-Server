f86cc2e22ae43c510ffc2975629f930c22b70d75
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-06-09 00:36:31 +0900

    feat: get random flower

---
+++ src/main/java/com/twoCube/gifts/dto/FlowerResponse.java
@@ -0,0 +1,11 @@
+package com.twoCube.gifts.dto;
+
+import lombok.Getter;
+
+@Getter
+public class FlowerResponse {
+    private long flowerId;
+    private String meaning;
+    private String flowerName;
+    private String flowerImage;
+}
