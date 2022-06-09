3a57f28e7d10bad1f36345c2aa9973fb42c37d1a
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-06-09 11:45:36 +0900

    feat: add testPosts

--- src/main/java/com/twoCube/test/Controller.java
+++ src/main/java/com/twoCube/test/Controller.java
@@ -6,9 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Test;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/testing")
@@ -17,11 +15,28 @@ public class Controller {
 
     @GetMapping("/test")
     @ApiOperation(value = "연동 테스트용 api")
-    public ResponseEntity<TestDto> createNote(
+    public ResponseEntity<TestDto> getTest(
 //                                           @ApiIgnore @CurrentUser User user
     ){
         TestDto test = new TestDto();
         test.setText("성공");
         return ResponseEntity.ok(test);
+    }
+
+    @PostMapping("/test")
+    @ApiOperation(value = "연동 테스트용 api")
+    public ResponseEntity<Long> createTest(@RequestBody TestDto testDto
+//                                           @ApiIgnore @CurrentUser User user
+    ){
+        System.out.println(testDto.getText());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/test/hasReturn")
+    @ApiOperation(value = "연동 테스트용 api")
+    public ResponseEntity<TestDto> postTest(@RequestBody TestDto testDto
+//                                           @ApiIgnore @CurrentUser User user
+    ){
+        return ResponseEntity.ok(testDto);
     }
 }
