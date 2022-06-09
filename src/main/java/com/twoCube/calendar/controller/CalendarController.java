b09acaaf33f3ded95f0c9d0c7b27d052f8da3894
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-06-09 18:49:53 +0900

    feat: getDayGift api

--- src/main/java/com/twoCube/calendar/controller/CalendarController.java
+++ src/main/java/com/twoCube/calendar/controller/CalendarController.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/calendar")
@@ -19,6 +16,14 @@ public class CalendarController {
 
     @PostMapping("/")
     @ApiOperation(value = "기념일 생성하기 api")
+    public ResponseEntity<Long> createNote(@RequestBody AnniversaryRequest anniversaryRequest
+//                                           @ApiIgnore @CurrentUser User user
+    ){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/day")
+    @ApiOperation(value = "하루 보기 api")
     public ResponseEntity<Long> createNote(@RequestBody AnniversaryRequest anniversaryRequest
 //                                           @ApiIgnore @CurrentUser User user
     ){
