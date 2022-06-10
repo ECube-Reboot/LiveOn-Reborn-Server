95a6c613512d58e6239ba23367dcd322c650f1e5
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-06-10 18:11:47 +0900

    chore: allow access to swagger without token

--- src/main/java/com/twoCube/config/SecurityConfig.java
+++ src/main/java/com/twoCube/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs/**", "/configuration/**", "/swagger-resources/**",
+        web.ignoring().antMatchers("/v3/api-docs/**", "/configuration/**", "/swagger-resources/**",
                 "/swagger-ui/**", "/webjars/**","/h2-console/**", "/oauth/**");
     }
 
