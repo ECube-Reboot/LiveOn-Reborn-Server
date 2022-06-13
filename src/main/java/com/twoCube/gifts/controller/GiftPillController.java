3e0dc06a535871d51c3c17628b990914ec2d44e8
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-06-13 19:46:49 +0900

    refactor: extract giftPolaroid controller

---
+++ src/main/java/com/twoCube/gifts/controller/GiftPillController.java
@@ -0,0 +1,39 @@
+package com.twoCube.gifts.controller;
+
+import com.twoCube.common.annotation.CurrentUser;
+import com.twoCube.gifts.dto.PillListResponse;
+import com.twoCube.gifts.dto.PillRequest;
+import com.twoCube.gifts.service.GiftPillService;
+import com.twoCube.members.domain.Member;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/gifts/pills")
+@RequiredArgsConstructor
+@Api(tags = {"알약 선물 API"})
+public class GiftPillController {
+    private final GiftPillService giftPillService;
+
+    @PostMapping("")
+    @ApiOperation(value = "영양제 선물하기 api")
+    public ResponseEntity<Long> createPill(@RequestBody PillRequest pillRequest,
+                                           @ApiIgnore @CurrentUser Member member
+    ) {
+        Long giftPillId = giftPillService.createPill(pillRequest, member);
+        return ResponseEntity.ok(giftPillId);
+    }
+
+    @GetMapping("")
+    @ApiOperation(value = "영양제 list 호출 api")
+    public ResponseEntity<List<PillListResponse>> getPillList() {
+        List<PillListResponse> pillListResponse = giftPillService.getPillList();
+        return ResponseEntity.ok(pillListResponse);
+    }
+}
---
+++ src/main/java/com/twoCube/gifts/controller/GiftPolaroidController.java
@@ -0,0 +1,34 @@
+package com.twoCube.gifts.controller;
+
+import com.twoCube.common.annotation.CurrentUser;
+import com.twoCube.gifts.service.GiftPolaroidService;
+import com.twoCube.members.domain.Member;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequestMapping("/api/v1/gifts/polaroids")
+@RequiredArgsConstructor
+@Api(tags = {"선물 API"})
+public class GiftPolaroidController {
+
+    private final GiftPolaroidService giftPolaroidService;
+
+    @PostMapping("")
+    @ApiOperation(value = "폴라로이드 선물하기 api")
+    public ResponseEntity<Long> createPolaroid(@RequestPart(required = false) MultipartFile polaroid,
+                                               @RequestPart(required = false, value = "comment") String comment,
+                                               @ApiIgnore @CurrentUser Member member
+    ) {
+        long giftPolaroidId = giftPolaroidService.createPolaroid(polaroid, comment, member);
+        return ResponseEntity.ok(giftPolaroidId);
+    }
+}
