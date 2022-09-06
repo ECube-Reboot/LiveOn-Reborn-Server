5b0675eae72016a1a828c2a3558e525cf5c265d6
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-09-06 15:11:21 +0900

    log 남김

--- src/main/java/com/twoCube/common/S3Uploader.java
+++ src/main/java/com/twoCube/common/S3Uploader.java
@@ -92,6 +92,9 @@ public class S3Uploader {
     }
 
     private Optional<File> convert(MultipartFile file) throws IOException {
+        System.out.println("converting");
+        System.out.println(file.getOriginalFilename());
+        lof.info(file.getOriginalFilename());
 
         File convertFile = new File(System.getProperty("java.io.tmpdir") +
                 System.getProperty("file.separator") +
